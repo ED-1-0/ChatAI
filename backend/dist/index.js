6f6fa2b3d977bd842571ae3c791aca7448305471
ED-1-0 <esak9721@gmail.com>
2023-11-07 17:19:43 -0800

    Updated backend

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,21 +1,16 @@
 import app from "./app.js";
 import { connectToDatabase } from "./db/connection.js";
-
-
 // HTTP verbs
 // GET - get some data from backend
 // PUT - update or modify or mutate some data
 // POST - send some data to create a new blog
 // DELETE - send some data to delete
-
 // connections and listeners
 const PORT = process.env.PORT || 4000;
 connectToDatabase()
     .then(() => {
-        app.listen(PORT, () => 
-            console.log("Server Open & Connected to Database")
-        );
-    })
+    app.listen(PORT, () => console.log("Server Open & Connected to Database"));
+})
     .catch(err => console.log(err));
-
 // run backend - npm run dev
+//# sourceMappingURL=index.js.map
---
+++ backend/src/db/connection.ts
@@ -0,0 +1,22 @@
+import { connect, disconnect } from "mongoose";
+
+async function connectToDatabase() {
+    try {
+        await connect(process.env.MONGODB_URL);
+    } catch (error) {
+        console.log(error);
+        throw new Error("Could not connect to MongoDB");
+    }
+}
+
+async function disconnectToDatabase() {
+    try {
+        await disconnect();
+    } catch (error) {
+        console.log(error);
+        throw new Error("Could not disconnect to MongoDB");
+    }
+    
+}
+
+export { connectToDatabase, disconnectToDatabase };
