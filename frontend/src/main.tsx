23295af715c18b80496f5309a0a964ee00034106
ED-1-0 <esak9721@gmail.com>
2023-11-16 18:43:08 -0800

    designed login and chat page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,12 @@ import App from './App.tsx'
 import './index.css'
 import { createTheme,ThemeProvider } from '@mui/material'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './context/AuthContest.tsx'
+import { AuthProvider } from './context/AuthContext.tsx'
+import { Toaster } from 'react-hot-toast'
+import axios from 'axios'
+
+axios.defaults.baseURL = "http://localhost:4000/api/v1";
+axios.defaults.withCredentials = true;
 
 const theme = createTheme({
   typography: {
@@ -17,6 +22,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <AuthProvider>
       <BrowserRouter>
         <ThemeProvider theme={theme}>
+          <Toaster position='top-right' />
           <App />
         </ThemeProvider>
       </BrowserRouter>
